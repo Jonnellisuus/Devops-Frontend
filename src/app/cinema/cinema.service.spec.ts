93371a4f689ee6e7f3094a82247a7c90982983ba
JKKiukas <42929741+JKKiukas@users.noreply.github.com>
2021-02-26 16:02:17 +0200

    Update cinema.service.spec.ts

--- src/app/cinema/cinema.service.spec.ts
+++ src/app/cinema/cinema.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CinemaService } from './cinema.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {News} from './news';
+import {HttpClient, HttpEvent, HttpEventType} from '@angular/common/http';
 
 describe('CinemaService', () => {
-  let service: CinemaService;
+  let cinemaService: CinemaService;
+  let cinemaNews: News;
+  let httpTestingController: HttpTestingController;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CinemaService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CinemaService]
+    });
+    cinemaService = TestBed.inject(CinemaService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('getData()', () => {
+    it('should return array of movie news', () => {
+      cinemaNews = {title: 'Title', publishDate: '26.2.2020', HTMLContent: 'HTMLContent', imageURL: 'imageURL'};
+      cinemaService.getData().subscribe((event: HttpEvent<any>) => {
+        switch (event.type) {
+          case HttpEventType.Response:
+            expect(event.body).toEqual(cinemaNews);
+        }
+      });
+
+      const mockReq = httpTestingController.expectOne(cinemaService.url);
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('text');
+      mockReq.flush(cinemaNews);
+
+      httpTestingController.verify();
+    });
   });
 });
